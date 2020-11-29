eee15fbf8a26aba43edcf4b49c741b89fdc6c654
Jérémy Dupuis <jeremy.dupuis10@gmail.com>
2020-11-29 14:12:31 +0100

    add datas in data.sql : project works

---
+++ trello-backend/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into developers (id, nom, prenom, avatar) values
+(1, 'Dupuis', 'Jérémy', 'aa');
+
+insert into bugs (id, name, description, priority, progress, creation_date, developers_id) values
+(1, 'bug 1', 'description du bug 1', 'normale', 'todo', '2020-11-29', 1);
+
+insert into commentaire(id, texte, bug_id, developers_id) values
+(1, 'commentaire 1 bug 1', 1, 1);
